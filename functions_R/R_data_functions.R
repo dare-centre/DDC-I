8a5b2f987bb83e07ac34a608af1eff684b402625
Travis Stenborg@ <travis.stenborg@sydney.edu.au>
2023-05-09 14:58:05 +1000

    Updated to match Python code.

--- functions_R/R_data_functions.R
+++ functions_R/R_data_functions.R
@@ -2,51 +2,62 @@
 ###############################################################################
 ###############################################################################
 
-load_raw_data <- function() {
+# Legacy function, currently disabled.
 
-  # Define a NULL variable reference to provide more graceful error
-  # handling in the event of failed data import.
-  raw_data <- NULL
-
-  # N.B. Assumes a header row is present.
-  raw_data <- readr::read_csv(
-    file = Gmisc::pathJoin(
-      funr::get_script_path(), "data", "raw_data",
-      "MiningProcess_Flotation_Plant_Database.csv"
-    ),
-    locale = readr::locale(decimal_mark = ","),
-    show_col_types = FALSE
-  )
-
-  return(raw_data)
-}
+#load_raw_data <- function() {
+#
+#  # Define a NULL variable reference to provide more graceful error
+#  # handling in the event of failed data import.
+#  raw_data <- NULL
+#
+#  # N.B. Assumes a header row is present.
+#  raw_data <- readr::read_csv(
+#    file = Gmisc::pathJoin(
+#      funr::get_script_path(), "data", "raw_data",
+#      "MiningProcess_Flotation_Plant_Database.csv"
+#    ),
+#    locale = readr::locale(decimal_mark = ","),
+#    show_col_types = FALSE
+#  )
+#
+#  return(raw_data)
+#}
 
 ###############################################################################
 ###############################################################################
 
-load_hourly_data <- function() {
+load_daily_data <- function() {
 
   # Load the hourly data
 
-  train_data <- readr::read_csv(
+  train_x <- readr::read_csv(
     file = Gmisc::pathJoin(
-      funr::get_script_path(), "data",
-      "hourly_train_data.csv"
+      here::here(), "data",
+      "daily_train_X_data.csv"
     ),
     show_col_types = FALSE
   )
-  test_data <- readr::read_csv(
+  train_y <- readr::read_csv(
     file = Gmisc::pathJoin(
-      funr::get_script_path(), "data",
-      "hourly_test_data.csv"
+      here::here(), "data",
+      "daily_train_y_data.csv"
     ),
     show_col_types = FALSE
   )
-
-  train_x <- train_data[, !(names(train_data) %in% "% Silica Concentrate")]
-  train_y <- train_data[, "% Silica Concentrate"]
-  test_x <- test_data[, !(names(test_data) %in% "% Silica Concentrate")]
-  test_y <- test_data[, "% Silica Concentrate"]
+  test_x <- readr::read_csv(
+    file = Gmisc::pathJoin(
+      here::here(), "data",
+      "daily_test_X_data.csv"
+    ),
+    show_col_types = FALSE
+  )
+  test_y <- readr::read_csv(
+    file = Gmisc::pathJoin(
+      here::here(), "data",
+      "daily_test_y_data.csv"
+    ),
+    show_col_types = FALSE
+  )
 
   return(list("train_x" = train_x, "train_y" = train_y,
               "test_x" = test_x, "test_y" = test_y))
